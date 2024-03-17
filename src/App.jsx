56fe9914fc4c69e625f26d837219f4fc37bda887
haykay36 <akeemayinla@gmail.com>
2024-03-17 12:11:24 -0500

    sign-up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Login from "./page/login/login";
+import Register from "./page/register/register";
 
 function App() {
-  return <div>
-    <Login />
-  </div>;
+  return (
+    <div>
+      <Register />
+
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- src/page/register/register.jsx
+++ src/page/register/register.jsx
@@ -1,5 +1,49 @@
+import AuthWrapper from "../../component/auth/AuthWrapper";
+import CustomInput from "../../component/inputs/custominput/CustomInput";
+import "./register.css";
+
 const Register = () => {
-  return <div>register</div>;
+  return (
+    <AuthWrapper>
+      <div className="signUp-cont">
+        <form>
+          <h2>Sign up</h2>
+          <CustomInput
+            type="text"
+            label="Full name"
+            placeholder="Ali Uthman"
+            htmlFor="Full name"
+          />
+          <CustomInput
+            type="Email"
+            label="Email"
+            placeholder="user@gmail.com"
+            htmlFor="email"
+          />
+          <CustomInput
+            type="password"
+            label="Password"
+            placeholder="********"
+            htmlFor="password"
+          />
+          <CustomInput
+            type="password"
+            label="Confirm Password"
+            placeholder="********"
+            htmlFor="password"
+          />
+          <div className="btn-con">
+            <button type="submit" className="submit-bt">
+              LOGIN
+            </button>
+            <div className="sign-in">
+               <a>Already have an account ? sign in</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </AuthWrapper>
+  );
 };
 
 export default Register;
