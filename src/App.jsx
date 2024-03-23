99e3c324098536019fb7e5296dde3eca8a8d0b34
haykay36 <akeemayinla@gmail.com>
2024-03-23 06:35:21 -0500

    updated register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { Route, Router, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./page/login/login";
 import Register from "./page/register/register";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
+        <Route path="/login" component={<Login />} />
         <Route path="/register">
           <Register />
         </Route>
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
