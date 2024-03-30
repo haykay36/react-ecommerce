832703f15389925e962dc6c065752e1143e1b808
haykay36 <tmemunat881@gmail.com>
2024-03-30 12:45:02 +0100

    description

---
+++ src/page/description/decription.jsx
@@ -0,0 +1,149 @@
+import React from "react";
+import MainWarpper from "../../component/layput/MainWrapper/MainWarpper";
+import "../../component/layput/MainWrapper/mainwrapper.css";
+
+import "./description.css";
+
+function Description() {
+  return (
+    <div>
+      <MainWarpper>
+        <div>
+          <div className="body-cont">
+            <div className="rice-cont">
+              <img src="./src/assets/6m 1.png" alt="" />
+            </div>
+            <div className="source-cont">
+              <h2 className="good">Good source of energy and Easy to Digest</h2>
+              <div className="desc">
+                <h3 className="brand">Brand</h3>
+                <div className="buttons">
+                  <button className="btn">Mama Pride</button>
+                  <button className="btn">Lake Rice</button>
+                  <button className="btn">Mama Gold</button>
+                </div>
+                <h3 className="price">Price</h3>
+                <h1>N1250.00</h1>
+              </div>
+              <div className="desc1">
+                <h3 className="color">Color: White</h3>
+                <div className="svg">
+                  <svg
+                    width="63"
+                    height="70"
+                    viewBox="0 0 63 70"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="63" height="70" rx="10" fill="#D9D9D9" />
+                    <ellipse
+                      cx="31.5"
+                      cy="35"
+                      rx="14.5"
+                      ry="14"
+                      fill="#D39E80"
+                    />
+                  </svg>
+
+                  <svg
+                    width="63"
+                    height="70"
+                    viewBox="0 0 63 70"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect
+                      x="1"
+                      y="1"
+                      width="61"
+                      height="68"
+                      rx="9"
+                      fill="#D9D9D9"
+                    />
+                    <rect
+                      x="1"
+                      y="1"
+                      width="61"
+                      height="68"
+                      rx="9"
+                      stroke="#0A3FF9"
+                      stroke-width="2"
+                    />
+                    <ellipse cx="31.5" cy="35" rx="14.5" ry="14" fill="white" />
+                  </svg>
+
+                  <svg
+                    width="63"
+                    height="70"
+                    viewBox="0 0 63 70"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="63" height="70" rx="10" fill="#D9D9D9" />
+                    <ellipse cx="31.5" cy="35" rx="14.5" ry="14" fill="black" />
+                  </svg>
+
+                  <svg
+                    width="63"
+                    height="70"
+                    viewBox="0 0 63 70"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="63" height="70" rx="10" fill="#D9D9D9" />
+                    <ellipse
+                      cx="31.5"
+                      cy="35"
+                      rx="14.5"
+                      ry="14"
+                      fill="#CB1D34"
+                    />
+                  </svg>
+                </div>
+                <h3 className="size">Size</h3>
+                <h1>10Kg</h1>
+              </div>
+            </div>
+          </div>
+          <div className="btn1-cont">
+            <button className="btn1">
+              <svg
+                width="18"
+                height="19"
+                viewBox="0 0 18 19"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.6248 6.25002H14.6248C13.9822 6.25002 13.4031 5.99736 12.9774 5.58598L12.4684 5.09401L11.8353 5.41059L6.27761 8.18935L5.646 8.50515L5.73231 9.20602C5.75636 9.40129 5.75636 9.59877 5.73231 9.79405L5.646 10.4949L6.27761 10.8107L11.8353 13.5895L12.4675 13.9056L12.9765 13.415C13.3798 13.0262 13.9077 12.7925 14.4667 12.7552C15.0257 12.718 15.5799 12.8795 16.0314 13.2113C16.4828 13.5431 16.8024 14.0238 16.9337 14.5684C17.065 15.113 16.9996 15.6866 16.7489 16.1876C16.4982 16.6886 16.0785 17.0849 15.5639 17.3065C15.0493 17.528 14.4729 17.5605 13.9367 17.3981C13.4004 17.2358 12.9389 16.8892 12.6335 16.4195C12.3281 15.9498 12.1986 15.3873 12.2679 14.8313L12.3553 14.1295L11.7228 13.8132L6.16512 11.0345L5.53272 10.7183L5.02377 11.2091C4.68828 11.5326 4.26501 11.7502 3.80668 11.8349C3.34836 11.9196 2.87525 11.8676 2.44629 11.6853C2.01732 11.5031 1.65147 11.1986 1.3943 10.81C1.13713 10.4213 1 9.96552 1 9.49947C1 9.03342 1.13713 8.57766 1.3943 8.18897C1.65147 7.80029 2.01732 7.49585 2.44629 7.31359C2.87525 7.13133 3.34836 7.07931 3.80668 7.164C4.26501 7.24869 4.68828 7.46635 5.02377 7.78987L5.53272 8.28064L6.16512 7.96446L11.7228 5.1857L12.3551 4.86953L12.2679 4.16793C12.2085 3.68937 12.2961 3.20404 12.5191 2.77648C12.7422 2.34892 13.0901 1.99939 13.5167 1.77438L13.0535 0.896293L13.5167 1.77438C13.9433 1.54937 14.4282 1.45953 14.9071 1.51682C15.3859 1.57412 15.836 1.77582 16.1974 2.09511C16.5588 2.4144 16.8145 2.83614 16.9304 3.30425C17.0463 3.77236 17.0169 4.26466 16.8462 4.71568L17.7815 5.06966L16.8462 4.71568C16.6755 5.16671 16.3716 5.55508 15.9748 5.82916C15.5779 6.10324 15.1071 6.25004 14.6248 6.25002Z"
+                  fill="black"
+                  stroke="#F5F5F5"
+                  stroke-width="2"
+                />
+              </svg>
+
+              <span>Share</span>
+            </button>
+            <button className="btn1">
+              <svg
+                width="21"
+                height="21"
+                viewBox="0 0 21 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0 0.25H2.27778L2.73333 2.52778M4.55556 11.6389H15.9444L20.5 2.52778H2.73333M4.55556 11.6389L2.73333 2.52778L4.55556 11.6389ZM4.55556 11.6389L1.94408 14.2504C1.22658 14.9679 1.73453 16.1944 2.74928 16.1944H15.9444M15.9444 16.1944C15.3403 16.1944 14.761 16.4344 14.3338 16.8616C13.9066 17.2888 13.6667 17.8681 13.6667 18.4722C13.6667 19.0763 13.9066 19.6557 14.3338 20.0829C14.761 20.51 15.3403 20.75 15.9444 20.75C16.5485 20.75 17.1279 20.51 17.5551 20.0829C17.9822 19.6557 18.2222 19.0763 18.2222 18.4722C18.2222 17.8681 17.9822 17.2888 17.5551 16.8616C17.1279 16.4344 16.5485 16.1944 15.9444 16.1944ZM6.83333 18.4722C6.83333 19.0763 6.59335 19.6557 6.16619 20.0829C5.73902 20.51 5.15966 20.75 4.55556 20.75C3.95145 20.75 3.37209 20.51 2.94492 20.0829C2.51776 19.6557 2.27778 19.0763 2.27778 18.4722C2.27778 17.8681 2.51776 17.2888 2.94492 16.8616C3.37209 16.4344 3.95145 16.1944 4.55556 16.1944C5.15966 16.1944 5.73902 16.4344 6.16619 16.8616C6.59335 17.2888 6.83333 17.8681 6.83333 18.4722Z"
+                  fill="#FEFEFA"
+                />
+              </svg>
+              <span>Add to Cart</span>
+            </button>
+          </div>
+        </div>
+      </MainWarpper>
+    </div>
+  );
+}
+
+export default Description;
